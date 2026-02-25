3373dfaee891b28ab78a28d48ce47d7b2cbe820b
Laurence Lama <laurencelamaFSE@gmail.com>
2026-02-25 16:55:01 +0800

    Padding on login page for phones

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginButton from '../components/LoginButton'
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-indigo-960">
+    <div className="min-h-screen flex items-center justify-center bg-indigo-960 px-6">
       <div className="bg-gray-200 p-8 rounded-xl shadow-lg w-96">
         <h1 className="text-indigo-950 text-2xl font-bold mb-6 text-center">
           Client Dashboard
